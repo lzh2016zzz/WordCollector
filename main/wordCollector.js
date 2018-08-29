0866d6583a14d2c3baf6941958b5321f111e592c
user <user@FJREACHJSB006.fjreach.com>
2018-08-29 16:57:50 +0800

    2018年8月29日16:57:49

--- main/wordCollector.js
+++ main/wordCollector.js
@@ -14,13 +14,13 @@ WordCollector.prototype.substr = function (str) {
 
 WordCollector.prototype.sort = function (subStrIdxArray, str) {
     if (subStrIdxArray.length <= 0 || !str) return [];
-    var pivotIndex = subStrIdxArray[Math.floor(subStrIdxArray.length / 2)];
-    var pivot = str.substring(pivotIndex);
-    var left = [];
-    var right = [];
-    var equal = [];
-    for (var i = 0; i < subStrIdxArray.length; i++) {
-        var compareValue = str.substring(subStrIdxArray[i]).localeCompare(pivot,'zh');
+    let pivotIndex = subStrIdxArray[Math.floor(subStrIdxArray.length / 2)];
+    let pivot = str.substring(pivotIndex);
+    let left = [];
+    let right = [];
+    let equal = [];
+    for (let i = 0; i < subStrIdxArray.length; i++) {
+        let compareValue = str.substring(subStrIdxArray[i]).localeCompare(pivot,'zh');
         if (compareValue < 0) left.push(subStrIdxArray[i]);
         else if (compareValue > 0) right.push(subStrIdxArray[i]);
         else equal.push(subStrIdxArray[i]);
@@ -33,17 +33,17 @@ WordCollector.prototype.PATArray = function (str) {
 }
 
 WordCollector.prototype.LCPArray = function (patArray, str) {
-    var ret = [];
+    let ret = [];
     if (patArray.length <= 0 || !str) return ret;
-    for (var i = 0; i < patArray.length; ++i) {
+    for (let i = 0; i < patArray.length; ++i) {
         let lcp = 0;
         if (i == patArray.length - 1) {
             ret.push(lcp);
             break;
         }
-        var str1 = str.substring(patArray[i]);
-        var str2 = str.substring(patArray[i + 1]);
-        for (var j = 0; j < str1.length; ++j) {
+        let str1 = str.substring(patArray[i]);
+        let str2 = str.substring(patArray[i + 1]);
+        for (let j = 0; j < str1.length; ++j) {
             if (str1.charAt(j) == str2.charAt(j)) lcp++;
             else break;
         }
@@ -53,23 +53,25 @@ WordCollector.prototype.LCPArray = function (patArray, str) {
 }
 
 WordCollector.prototype.scan = function (lcpArray, patArray, str, idx, minLen, maxLen) {
-    var ret = [];
-    for (var i = idx; i < lcpArray.length; ++i) {
-        var lcp = lcpArray[i];
-        var start = i + 1;
-        if (lcp < minLen || i > 0 && lcpArray[i-1] == lcp) {
+    let ret = [];
+    for (let i = idx; i < lcpArray.length; ++i) {
+        let lcp = lcpArray[i] > maxLen ? maxLen : lcpArray[i];
+        let last = lcpArray[i-1] > maxLen ? maxLen : lcpArray[i-1];
+        let start = i + 1;
+        if (lcp < minLen || i > 0 &&lcp == last) {
             continue;
         }
         else if(lcp > maxLen){
             lcp = maxLen;
         }
-        var text = str.substring(patArray[i]).substring(0, lcp);
-        var count = 0;
-        var k = start;
+        let text = str.substring(patArray[i]).substring(0, lcp);
+        let count = 0;
+        let k = start;
         for (; k < lcpArray.length; ++k) {
-            if (lcpArray[k] < lcp) {
+            let next = lcpArray[k] > maxLen ? maxLen : lcpArray[k];
+            if (next < lcp) {
                 count = k - i + 1;
-                var x = 1;
+                let x = 1;
                 while (i != 0 && lcpArray[i - x] > lcp) {
                     ++count;
                     ++x;
